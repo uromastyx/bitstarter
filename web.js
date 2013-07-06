8719dbc08a550ea52ca570eb72e85c3b4f1d0ff9
Johnny Bergmann <jbergmann@hotmail.com>
2013-07-06 01:03:15 -0500

    fixed read from file

--- web.js
+++ web.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+console.log("about to start FS");
   var fs = require('fs');
-  var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("./index.html")))
+console.log("about to start new buffer");
+  var mybuffer = new Buffer(16);
+console.log("about to read file ");
+mybuffer=fs.readFileSync("index.html");
+console.log(mybuffer.toString());
+  response.send(mybuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
