f4b0e2ef0df046e356531446a91c12d565050e7d
Johnny Bergmann <jbergmann@hotmail.com>
2013-07-05 23:58:09 -0500

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var buffer = new buffer ();
 
 app.get('/', function(request, response) {
-var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))
+  response.send(buffer.toString('utc 8', fs.readFileSync("./index.html")))
 });
 
 var port = process.env.PORT || 5000;
